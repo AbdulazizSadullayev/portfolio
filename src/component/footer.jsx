1d4040d73b3c87520a34f211ab71f26df41b3a08
Abdulaziz SAdullayev <sadullayevabdulaziz2009@gmail.com>
2025-10-27 16:45:18 +0500

    first commit

---
+++ src/component/footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return <footer>
+    <div className="container">
+      <p className="text-center text-gray-500 py-5">
+        &copy; {new Date().getFullYear()} Abdulaziz's Blog
+      </p>
+    </div>
+  </footer>
+};
+
+export default Footer;
---
+++ src/data/index.js
@@ -0,0 +1,39 @@
+import github from '../assets/github.svg'
+import linkedin from '../assets/linkedin.svg'
+import telegram from '../assets/telegram.svg'
+
+export const headerLinks = [
+  {
+    id: 0,
+    text: "Projects",
+    to: "/projects",
+  },
+  {
+    id: 1,
+    text: "Articles",
+    to: "/articles",
+  },
+  {
+    id: 2,
+    text: "Contact",
+    to: "/contact",
+  },
+];
+
+export const bannerLinks = [
+  {
+    id: 0,
+    icon: github,
+    to: "https://github.com/AbdulazizSadullayev "
+  },
+  {
+    id: 1,
+    icon: linkedin,
+    to: "https://www.linkedin.com/in/abdulaziz-sadullayev-66b705395?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
+  },
+  {
+    id: 2,
+    icon: telegram,
+    to: "https://t.me/abdulazizSadullayev"
+  },
+]
